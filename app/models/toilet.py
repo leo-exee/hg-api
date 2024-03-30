734d064d52b6658d935cb33168a765a2c4eb9ec4
leo-exee <leopold@littlebill.io>
2024-03-30 11:13:51 +0100

    feat(toilet): Add toilet model

---
+++ app/models/toilet.py
@@ -0,0 +1,64 @@
+from enum import Enum
+from pydantic import BaseModel
+from app.models.mongo import MongoModel, PyObjectId
+
+
+class lockSystemEnum(str, Enum):
+    MANUAL = "manual"
+    AUTOMATIC = "automatic"
+    NO_LOCK = "no_lock"
+
+
+class DaysOfWeekEnum(str, Enum):
+    MONDAY = "monday"
+    TUESDAY = "tuesday"
+    WEDNESDAY = "wednesday"
+    THURSDAY = "thursday"
+    FRIDAY = "friday"
+    SATURDAY = "saturday"
+    SUNDAY = "sunday"
+
+
+class Review(BaseModel):
+    rating: int
+    state: int
+    cleanliness: int
+    accessbility: int
+    comment: str
+
+
+class Location(BaseModel):
+    lat: float
+    long: float
+
+
+class OpeningInfo(BaseModel):
+    day: DaysOfWeekEnum
+    allDay: bool
+    allHours: bool
+    openHour: int
+    closeHour: int
+
+
+class Information(BaseModel):
+    rating: int
+    state: int
+    cleanliness: int
+    accessbility: int
+    babyFriendly: bool
+    handicapFriendly: bool
+    lockSystem: lockSystemEnum
+    openingHours: list[OpeningInfo]
+    maintenancePhoneNum: str
+
+
+class ToiletInDAO(MongoModel):
+    name: str
+    address: str
+    location: Location
+    information: Information
+    reviews: list[Review]
+
+
+class ToiletOutDTO(ToiletInDAO):
+    id: PyObjectId
